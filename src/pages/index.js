d5a963543138512a1e101a9ae067b808c2eaf80c
pavelkrolevets <pavelkrolevets@gmail.com>
2023-04-21 13:53:12 +0300

    update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} - модульный блокчейн`}
+      description="MIR это первый открытый ГОСТ совместимый блокчйен">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
